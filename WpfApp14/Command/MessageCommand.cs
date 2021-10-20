2786aabf27753dda68da5308caafec5cca9bef22
User <User@REVOLT>
2021-10-21 00:21:47 +0400

    Add project files.

---
+++ WpfApp14/Command/MessageCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp14.Command
+{
+    public class MessageCommand : ICommand
+    {
+        //public event EventHandler CanExecuteChanged;
+
+        //private Action _action;
+
+        //public MessageCommand(Action action)
+        //{
+        //    _action = action;
+        //}
+
+        //public bool CanExecute(object parameter)
+        //{
+        //    MessageBox.Show("a");
+        //    return true;
+        //}
+
+        //public void Execute(object parameter)
+        //{
+        //    _action.Invoke();
+        //}
+        public event EventHandler CanExecuteChanged;
+        private Action _action;
+
+        public MessageCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action.Invoke();
+        }
+    }
+}
---
+++ WpfApp14/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp14.Command;
+
+namespace WpfApp14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MessageCommand MessageCommand;
+        public string Text { get; set; } = "Salam";
+        public MainWindow()
+        {
+            this.DataContext = this;
+            MessageCommand = new MessageCommand(Display);
+            InitializeComponent();
+        }
+        public void Display()
+        {
+            MessageBox.Show("Hatuna Matata");
+        }
+        public void Send()
+        {
+            MessageBox.Show("Message was send successfully");
+        }
+    }
+}
